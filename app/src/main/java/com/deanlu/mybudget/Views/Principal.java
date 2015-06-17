021c5b6ef3034ebc72c670560c333c4af2232656
Deimer Villa <Deimer Villa>
2015-06-16 23:12:50 -0500

    Primer commit del proyecto

---
+++ app/src/main/java/com/deanlu/mybudget/Views/Principal.java
@@ -0,0 +1,30 @@
+package com.deanlu.mybudget.Views;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.deanlu.mybudget.R;
+
+/**
+ * Created by Deimer on 15/06/2015.
+ * ----------------------------------------------------
+ * Clase controladora de la vista principal
+ * encargada de manejar y gestionar los procesos
+ * de la vista principal
+ */
+public class Principal extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return true;
+    }
+
+}
