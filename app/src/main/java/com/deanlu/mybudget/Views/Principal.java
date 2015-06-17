57d062ccf4f38347380d78569e1e62b6d5883dcd
Deimer Villa <Deimer Villa>
2015-06-16 23:22:04 -0500

    Cambios en la clase de la vista principal.

--- app/src/main/java/com/deanlu/mybudget/Views/Principal.java
+++ app/src/main/java/com/deanlu/mybudget/Views/Principal.java
@@ -18,7 +18,6 @@ public class Principal extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_principal);
     }
 
